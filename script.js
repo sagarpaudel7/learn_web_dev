c84928d636a24a07e71ee25d249e6361ce1a65eb
Sagar Paudel <sagarpaudel2058@gmail.com>
2024-01-21 17:34:28 +0545

    select html element by using document

---
+++ script.js
@@ -0,0 +1,9 @@
+//to get element by class
+document.getElementsByClassName("name");
+//to get by id
+document.getElementById("id1")
+;
+//to get by using like css  selector
+document.querySelector("h1");//for h1 tag
+//to select all element of same class 
+document.querySelectorAll("name"); 
